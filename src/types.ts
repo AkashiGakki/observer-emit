4df5f4a4153bc8f6a437ff04a71b92e8af620ffb
Akashi <akashi_sai@163.com>
2022-06-28 22:06:37 +0800

    ✨ feat: types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,7 @@
-
+export interface Observer {
+  list: Record<string, any[]>
+  on: (value: string, fn: () => void) => void
+  emit: (value: string) => void
+  remove: (value: string) => boolean
+  once: (value: string, fn: () => void) => void
+}
