d36b148f0bfe69b02045d1b0eb096d2011b28a26
Akashi <akashi_sai@163.com>
2022-06-30 21:50:41 +0800

    :white_check_mark: chore: test emitter

--- test/index.test.ts
+++ test/index.test.ts
@@ -11,7 +11,65 @@ describe('should', () => {
         "on": [Function],
         "once": [Function],
         "remove": [Function],
+        "removeAll": [Function],
       }
     `)
+  })
+
+  it('on', () => {
+    emitter.on('shiori', () => { })
+    expect(emitter.list).toMatchInlineSnapshot(`
+      {
+        "shiori": [
+          [Function],
+        ],
+      }
+    `)
+  })
+
+  it('emit', () => {
+    emitter.on('yama', () => { })
+    emitter.emit('mizuki')
+    expect(emitter.list).toMatchInlineSnapshot(`
+      {
+        "shiori": [
+          [Function],
+        ],
+        "yama": [
+          [Function],
+        ],
+      }
+    `)
+  })
+
+  it('once', () => {
+    emitter.once('yoda', () => { })
+    emitter.emit('yoda')
+    expect(emitter.list).toMatchInlineSnapshot(`
+      {
+        "shiori": [
+          [Function],
+        ],
+        "yama": [
+          [Function],
+        ],
+      }
+    `)
+  })
+
+  it('remove', () => {
+    emitter.remove('yama')
+    expect(emitter.list).toMatchInlineSnapshot(`
+      {
+        "shiori": [
+          [Function],
+        ],
+      }
+    `)
+  })
+
+  it('removeAll', () => {
+    emitter.removeAll()
+    expect(emitter.list).toMatchInlineSnapshot('{}')
   })
 })
